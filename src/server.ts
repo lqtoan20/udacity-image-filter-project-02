c35eddd8234ecbe4473c98ff4263e7c2180cf203
ToanLa <lqtoan20@gmail.com>
2023-03-19 16:57:10 +0700

    Implement method to filter an image from a public URL

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import express from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
+import { Router, Request, Response } from "express";
 
 (async () => {
-
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -28,19 +28,30 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  const HTTP_STATUS_OK = 200;
+  const HTTP_STATUS_NOT_FOUND = 404;
+  app.get("/filteredimage/", async (req: Request, res: Response) => {
+    const image_url = req.query.image_url;
+    if (image_url.length === 0) {
+      res.status(HTTP_STATUS_NOT_FOUND).send("Image URL is required!");
+    }
 
+    const filtered_image = await filterImageFromURL(image_url);
+    res.status(HTTP_STATUS_OK).sendFile(filtered_image, () => {
+      deleteLocalFiles([filtered_image]);
+    });
+  });
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}");
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
